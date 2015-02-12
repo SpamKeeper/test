721f9b3aedbfccad318b0b232bf9e7a737c8c05b
SpamKeeper <spam.keeper@mail.ru>
2015-02-12 13:56:50 +0600

    last static

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
-<?php 
-echo "Hello";
-print 'Good';
+
+<?php
+class A {
+    public static function who() {
+        echo __CLASS__;
+    }
+    public static function test() {
+        self::who();
+    }
+}
+
+class B extends A {
+    public static function who() {
+        echo __CLASS__;
+    }
+}
+
+B::test();
 ?>
